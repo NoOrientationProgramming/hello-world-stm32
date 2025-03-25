7c9ee7a3307f74e5b284c7a5d551981a747f5cf5
Johannes Natter <office@dsp-crowd.com>
2025-03-25 14:26:57 +0100

    cppcheck: Check target specific SystemDebugging() only

--- cppcheck.sh
+++ cppcheck.sh
@@ -5,6 +5,7 @@ find \
 	-type f \
 	-name "*.cpp" \
 	! -path "./ProcessingCore/stm32-uart/*" \
+	! -path "./ProcessingCore/SystemDebugging.*" \
 | xargs cppcheck \
 	--language=c++ \
 	--std=c++11 \
