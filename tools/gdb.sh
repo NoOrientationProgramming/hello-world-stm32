0e53ef1c598ae3398ead8c85bb3114232bb7443d
Johannes Natter <office@dsp-crowd.com>
2025-03-28 01:15:02 +0100

    tools: Create directory

---
+++ tools/gdb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#gdb-multiarch --quiet "src/patches/discman_4-1/build/discman_4-1.elf" --eval "target remote localhost:3333" --eval "monitor reset halt"
+
+d="build"
+p="$(ls -1 $d/*.elf)"
+
+gdb-multiarch --quiet "$p" -x gdb.cfg
+
