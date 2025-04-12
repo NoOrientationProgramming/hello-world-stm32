d1718f51401e082f2df04b3f18389961f605dfb9
Johannes Natter <office@dsp-crowd.com>
2025-04-12 11:53:38 +0200

    flash.sh: Fix renaming

--- flash.sh
+++ flash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#st-flash --format=ihex --connect-under-reset write ./build/stm32-hello-world.hex
-st-flash --format=ihex --reset write ./build/stm32-hello-world.hex
+#st-flash --format=ihex --connect-under-reset write ./build/hello-world-stm32.hex
+st-flash --format=ihex --reset write ./build/hello-world-stm32.hex
 
